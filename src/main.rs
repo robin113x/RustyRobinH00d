a1e988962bd5f131759decaf0e494e1294693976
Robin <robin113x@gmail.com>
2024-07-21 02:36:29 +0530

    Nice

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+use std::io::Read;
+
+fn main(){
+  let name = "ROBINHOOD";
+    //let ss = &name[3..5];
+    println!("{:?}",&name[2..5]);
+
+    let name = String::from("ISHITASINGH");
+    println!("{}",&name[1..6]);
+
+}
+
+
+
+
+
+
+
+
+
